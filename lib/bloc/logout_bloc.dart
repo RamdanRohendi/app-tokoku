87ce978e2812add05dce343a8779c5b19efc12a7
Ramdan Rohendi <ramdanrohendi@gmail.com>
2024-07-03 14:37:57 +0700

    fix(): Fungsi Logout sudah get pi

--- lib/bloc/logout_bloc.dart
+++ lib/bloc/logout_bloc.dart
@@ -1,7 +1,23 @@
+import 'dart:convert';
+
+import 'package:tokoku/helpers/api.dart';
 import 'package:tokoku/helpers/user_info.dart';
+import 'package:tokoku/helpers/api_url.dart';
 
 class LogoutBloc{
   static Future logout() async {
+    String apiUrl = ApiUrl.logout;
+    int? id = await UserInfo().getUserID();
+
+    var body = {
+      "id": id.toString(),
+    };
+
+    var response = await Api().post(apiUrl, body);
+    var jsonObj = json.decode(response.body);
+
     await UserInfo().logout();
+
+    return jsonObj['status'];
   }
 }
